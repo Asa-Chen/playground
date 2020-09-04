f95748188c774d91bd273b2ae444bc6677f01824
Asa-Chen <ac581218@ohio.edu>
2020-09-03 20:48:51 -0400

    Initial Hello_World.cc

---
+++ Hello_World.cc
@@ -0,0 +1,15 @@
+/**
+ * @author: Asa Chen
+ * @date: September 01, 2020
+ * @desc: A simple test file for C++ and Git.Hub
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
